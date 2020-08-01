925bfe09e55d2288a121cc316bf529087f06cf1e
gromitsun <gromitsun@users.noreply.github.com>
2020-08-01 10:27:02 -0400

    Add matrix operations: - Kronecker power - matrix exponential - partial trace - multi-controlled-unitary

--- analytical/basic_gates.py
+++ analytical/basic_gates.py
@@ -4,7 +4,43 @@ Matrix representation of basic gates
 import typing
 import numpy as np
 
+######################################
+# Pauli matrices
+######################################
+pauli_x = np.matrix([
+    [0, 1],
+    [1, 0]
+])
 
+
+pauli_y = np.matrix([
+    [0, -1j],
+    [1j, 0]
+])
+
+
+pauli_z = np.matrix([
+    [1, 0],
+    [0, -1]
+])
+
+
+ident = np.matrix([
+    [1, 0],
+    [0, 1]
+])
+
+
+def ident_n(n: int) -> np.matrix:
+    """
+    n-qubit identity matrix
+    """
+    return np.identity(2**n)
+
+
+######################################
+# Single-qubit rotations
+######################################
 def rx(theta: typing.SupportsFloat) -> np.matrix:
     return np.matrix([
         [np.cos(theta/2), -1j*np.sin(theta/2)],
@@ -26,19 +62,93 @@ def rz(theta: typing.SupportsFloat) -> np.matrix:
     ])
 
 
-pauli_x = np.matrix([
-    [0, 1],
-    [1, 0]
-])
+######################################
+# Derived gates / matrix operations
+######################################
+def kron_power(m: np.matrix, n: int) -> np.matrix:
+    """
+    :param m: matrix
+    :param n: positive integer
+    :return: Kronecker product of n copies of m, i.e.
+        m x m x ... x m
+    """
+    if n == 1:
+        return m
+    # divide and conquer
+    # divide
+    prod = kron_power(m, n//2)
+    # combine
+    prod = np.kron(prod, prod)
+    # handle case when n is odd
+    if n % 2 == 1:
+        prod = np.kron(m, prod)
+    return prod
 
 
-pauli_y = np.matrix([
-    [0, -1j],
-    [1j, 0]
-])
+def mcu(u: np.matrix, n: int, i: int = -1) -> np.matrix:
+    """
+    Multi-controlled-U operator
+    :param u: single-qubit (2x2) unitary matrix
+    :param n: total number of qubits
+    :param i: (optional) target qubit (0-based index)
+    :return: matrix representation of multi-controlled-U operator
+        targetted on the i-th qubit
+    """
+    # handle negative i
+    if i < 0:
+        i += n
+    # statics
+    a = (ident + pauli_z) / 2
+    b = (ident - pauli_z) / 2
+
+    # solve the problem recursively
+    def _mcu_recursive(u, n, i):
+        # trivial case
+        if n == 1:
+            return u
+        # general case
+        if i > 0:
+            return np.kron(a, ident_n(n-1)) \
+                + np.kron(b, _mcu_recursive(u, n-1, i-1))
+        return np.kron(ident_n(n-1), a) \
+            + np.kron(_mcu_recursive(u, n-1, i), b)
+    
+    return _mcu_recursive(u, n, i)
+
+    
+def exp_real(u: np.matrix, t: typing.SupportsFloat) -> np.matrix:
+    """
+    :param m: unitary matrix
+    :param t: time -- real number
+    :return: exp(i*u*t)
+    """
+    return np.cos(t)*ident_n(u.shape[0]) + 1j*np.sin(t)*u
 
 
-pauli_z = np.matrix([
-    [1, 0],
-    [0, -1]
-])
+def exp_imag(u: np.matrix, t: typing.SupportsFloat) -> np.matrix:
+    """
+    :param m: unitary matrix
+    :param t: time -- real number
+    :return: exp(u*t)
+    """
+    return np.cosh(t)*ident_n(u.shape[0]) + np.sinh(t)*u
+
+
+def partial_trace(
+    m: np.matrix,
+    i: typing.Union[int, typing.Sequence[int]]
+) -> typing.Union[typing.SupportsComplex, np.matrix]:
+    """
+    Take partial trace of a density matrix on the i-th qubits (0-based indices)
+    """
+    if not isinstance(i, int):
+        return partial_trace(partial_trace(m, i[1:]), i[0])
+
+    n_state = m.shape[0]
+    n_qubit = np.log2(n_state)
+    n_pre = 2**i
+    n_post = 2**(n_qubit-i-1)
+    return np.trace(
+        m.reshape(n_pre, 2, n_post, n_pre, 2, n_post),
+        axis1=1, axis2=4
+    ).reshape(n_state/2, n_state/2)
