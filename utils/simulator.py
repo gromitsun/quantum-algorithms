07b8e66f3d208d81e4ea506f3ec148848b61e0b9
gromitsun <gromitsun@users.noreply.github.com>
2020-08-24 17:16:13 -0400

    Switch from BasicAer to Aer simulators

--- utils/simulator.py
+++ utils/simulator.py
@@ -6,12 +6,12 @@ from utils.common import int_to_bin
 
 
 def get_statevector(circuit: qiskit.QuantumCircuit) -> np.ndarray:
-    backend = qiskit.BasicAer.get_backend('statevector_simulator')
+    backend = qiskit.Aer.get_backend('statevector_simulator')
     job = qiskit.execute(circuit, backend)
     return job.result().get_statevector()
 
 def get_counts(circuit: qiskit.QuantumCircuit, shots: typing.Union[int, None] = None) -> dict:
-    backend = qiskit.BasicAer.get_backend('qasm_simulator')
+    backend = qiskit.Aer.get_backend('qasm_simulator')
     job = qiskit.execute(circuit, backend, shots=shots)
     return job.result().get_counts()
 
@@ -23,6 +23,6 @@ def get_probabilities(circuit: qiskit.QuantumCircuit) -> dict:
     return sv_to_prob(sv)
 
 def get_unitary(circuit: qiskit.QuantumCircuit) -> np.ndarray:
-    backend = qiskit.BasicAer.get_backend('unitary_simulator')
+    backend = qiskit.Aer.get_backend('unitary_simulator')
     job = qiskit.execute(circuit, backend)
     return job.result().get_unitary()
